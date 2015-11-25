e5ca3675f8c5f95b7b3a21ddc64b856444d59be0
Hanna Eismant <hanna.eismant@gmail.com>
2015-11-25 11:26:56 +0300

    Create project base structure

---
+++ Grunt/Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function (grunt) {
+
+    // Волшебная строчка. Она творит магию.
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        wiredep: {
+            target: {
+                src: 'app/*.html'
+            }
+        },
+
+        // Эта таска отслеживает изменения в файлах проекта
+        // и запускает другие таски при необходимости.
+        watch:  {
+            // Волшебство. При изменении файлов открытая страничка
+            // в браузере перезагрузится автоматически.
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/**'
+                ]
+            }
+        },
+
+        // Эта таска запускает сервер.
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                livereload: 35729
+            },
+
+            livereload: {
+                open: true,
+                middleware: function (connect) {
+                    return [
+                        connect.static('app'),
+                        connect().use('/bower_components', connect.static('./bower_components'))
+                    ];
+                }
+            }
+        }
+
+    });
+
+    // Регестрируем таску, которую можно вызвать с консоли.
+    // По сути, под одним названием мы запускаем другие в указанном порядке.
+    grunt.registerTask('server', [
+        'connect',
+        'watch'
+    ]);
+
+};
