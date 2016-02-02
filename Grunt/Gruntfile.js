9d281994a4fb647b3d11a2d2f2a7a55997547ff1
Hanna Eismant <hanna.eismant@gmail.com>
2016-02-02 13:41:21 +0300

    Start server for dev

--- Grunt/Gruntfile.js
+++ Grunt/Gruntfile.js
@@ -1,56 +1,114 @@
 module.exports = function (grunt) {
 
-    // Волшебная строчка. Она творит магию.
-    require('load-grunt-tasks')(grunt);
+  // Autoload grunt tasks
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        wiredep: {
-            target: {
-                src: 'app/*.html'
-            }
+  var appConfig = {
+    app: require('./bower.json').appPath || 'app',
+    dist: 'dist',
+    tmp: '.tmp',
+    server: 'server'
+  };
+
+  grunt.initConfig({
+
+    config: appConfig,
+
+    // Cleanup directories
+    clean: {
+      server: '<%= config.server %>'
+    },
+
+    // Copy source files into another directory
+    copy: {
+      server: { // subtask for dev
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>',
+          src: ['**', '!**/*.less'],
+          dest: '<%= config.server %>/'
+        }]
+      }
+    },
+
+    // Include bower dependencies into index.html
+    wiredep: {
+      server: {
+        src: ['<%= config.server %>/index.html'],
+        ignorePath: /\.\.\//
+      }
+    },
+
+    // Watching project source files
+    watch: {
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
         },
+        files: [
+          '<%= config.server %>/**'
+        ]
+      }
+    },
 
-        // Эта таска отслеживает изменения в файлах проекта
-        // и запускает другие таски при необходимости.
-        watch:  {
-            // Волшебство. При изменении файлов открытая страничка
-            // в браузере перезагрузится автоматически.
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    'app/**'
-                ]
-            }
-        },
+    // Start grunt server
+    //connect: {
+    //  options: {
+    //    port: 9000,
+    //    hostname: 'localhost',
+    //    livereload: 35729,
+    //    open: false
+    //  },
+    //  livereload: {
+    //    options: {
+    //      open: false,
+    //      middleware: function (connect) {
+    //        return [
+    //          connect.static(appConfig.server), // Use files from tmp directory to run
+    //          connect().use('/bower_components', connect.static('./bower_components'))
+    //        ];
+    //      }
+    //    }
+    //  }
+    //}
 
-        // Эта таска запускает сервер.
-        connect: {
-            options: {
-                port: 9000,
-                hostname: 'localhost',
-                livereload: 35729
-            },
+    // Start server
+    connect: {
+      options: {
+        port: 9000,
+        //base: '<%= config.server %>',
+        hostname: 'localhost',
+        livereload: 35729
+      },
 
-            livereload: {
-                open: true,
-                middleware: function (connect) {
-                    return [
-                        connect.static('app'),
-                        connect().use('/bower_components', connect.static('./bower_components'))
-                    ];
-                }
-            }
+      livereload: {
+        open: true,
+        middleware: function (connect) {
+          return [
+            connect.static(appConfig.server),
+            connect().use('/bower_components', connect.static('./bower_components'))
+          ];
         }
+      }
+    }
 
-    });
+  });
 
-    // Регестрируем таску, которую можно вызвать с консоли.
-    // По сути, под одним названием мы запускаем другие в указанном порядке.
-    grunt.registerTask('server', [
-        'connect',
-        'watch'
-    ]);
+  /* Run server
+
+   - clean server directory
+   - copy sources into server directory
+   the remaining tasks use the files from the server directory
+   - include bower dependencies
+   - start grunt server
+   - start watching project source files
+   */
+  grunt.registerTask('server', [
+    'clean:server',
+    'copy:server',
+    'wiredep:server',
+    'connect',
+    'watch'
+  ]);
 
 };
